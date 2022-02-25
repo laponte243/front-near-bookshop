732af086581cee8e4e74033751db6205aa4789c1
Linda-Bley <lindaleyrosario@gmail.com>
2022-02-25 14:35:57 -0400

    market nft

--- src/views/dashboard/pages/market/market.vue
+++ src/views/dashboard/pages/market/market.vue
@@ -3,36 +3,40 @@
     <v-container class="pt-12 mt-12">
       <v-row class="ul1">
         <v-col
-          v-for="(item, i) in offers"
+          v-for="(item, i) in dataNftTokens"
           :key="i"
-          class="col-xs-12 col-sm-6 col-md-3 li1"
+          class="col-xs-12 col-sm-4 col-md-2 li1"
         >
-          <a href="/app#/pages/product">
-            <v-card
-              class="hover2"
-            >
+          <router-link :to="{ name: 'product', params: { idserie: item.token_series_id } }">
+            <v-card class="hover2">
               <v-img
-                :src="require('../../../../assets/img/' + item.img)"
+                :src="item.metadata.media"
                 class="white--text align-end imgOffers"
               />
-              <div
-                class="textHover"
-              >
-                <h2>
-                  {{ item.nombre }}
-                </h2>
-                <h3>
-                  {{ item.Author }}
-                </h3>
-                <h3>
-                  {{ item.price }}
-                </h3>
+              <div class="textHover pt-4">
+                <h4>
+                  {{ item.metadata.title }}
+                </h4>
+                <h5>
+                  <!--{{ item.metadata.price }}-->
+                  <strong>
+                    10 NEAR
+                  </strong>
+                </h5>
+                <h5>
+                  {{ item.metadata.copies }} copias
+                </h5>
+                <br>
+                <h5>
+                  {{ item.creator_id }}
+                </h5>
+                <br>
                 <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, optio. Placeat libero culpa veniam ipsam porro eum perferendis ex sed consectetur ipsa, explicabo recusandae voluptatibus provident. Doloribus earum ullam quidem?
+                  {{ item.metadata.description }}
                 </p>
               </div>
             </v-card>
-          </a>
+          </router-link>
         </v-col>
       </v-row>
     </v-container>
@@ -40,6 +44,10 @@
 </template>
 
 <script>
+  import * as nearAPI from 'near-api-js'
+  import { CONFIG } from '@/services/api'
+  const { connect, keyStores, WalletConnection, Contract } = nearAPI
+
   export default {
     name: 'DashboardDashboard',
 
@@ -76,68 +84,91 @@
             copy: '10 Copias',
           },
         ],
+        dataNftTokens: [],
       }
     },
     mounted () {
-
+      this.nftTokensContract()
     },
     methods: {
       viewProduct () {
         this.$router.push({ name: 'product' })
       },
+      async nftTokensContract () {
+        const CONTRACT_NAME = 'bookshop.testnet'
+        // connect to NEAR
+        const near = await connect(
+          CONFIG(new keyStores.BrowserLocalStorageKeyStore()),
+        )
+        // create wallet connection
+        const wallet = new WalletConnection(near)
+        const contract = new Contract(wallet.account(), CONTRACT_NAME, {
+          viewMethods: ['get_nft_series'],
+          sender: wallet.account(),
+        })
+        if (wallet.isSignedIn()) {
+          const response = await contract.get_nft_series({
+            from_index: '0',
+            limit: 10,
+          })
+          this.dataNftTokens = response
+          console.log(response)
+        }
+      },
     },
   }
 </script>
 <style>
-  .ul1 {
-    list-style: none;
-  }
-  .li1 {
-    display: inline-block;
-  }
-  .imgOffers {
-    filter: brightness(50%);
-    -webkit-filter: brightness(50%);
-    -moz-filter: brightness(50%);
-    -o-filter: brightness(50%);
-    -ms-filter: brightness(50%);
-    height: 400px;
-  }
-  .hover2:hover {
-    display:inline-block;
-    background: #6868ac;
-  }
+.ul1 {
+  list-style: none;
+}
+.li1 {
+  display: inline-block;
+}
+.imgOffers {
+  filter: brightness(50%);
+  -webkit-filter: brightness(50%);
+  -moz-filter: brightness(50%);
+  -o-filter: brightness(50%);
+  -ms-filter: brightness(50%);
+  height: 300px;
+  width: 180px;
+}
+.hover2:hover {
+  display: inline-block;
+  background: #6868ac;
+}
 
-  .imgOffers:hover {
-    opacity:.3;
-  }
-  .textHover {
-    display: none;
-  }
-  .hover2 .imgOffers:hover ~ .textHover {
-    display: inline-block;
-    color: #fff;
-    position: absolute;
-    z-index: 1;
-    top: 0;
-    padding-left: 10px;
-  }
-  @media (min-width:320px) and (max-width:425px){
-    /*Aquí van todos los estilos CSS*/
-  };
-  @media (min-width:375px) and (max-width:425px){
-    /*Aquí van todos los estilos CSS*/
-  }
-  @media (max-width:768px) {
-    /*Aquí van todos los estilos CSS*/
-  }
-  @media (min-width:960px) and (max-width:1024px){
-    /*Aquí van todos los estilos CSS*/
-  };
-  @media (orientation: landscape) {
-    /*Aquí van todos los estilos CSS*/
-  }
-  @media (orientation: Portrait) {
-    /*Aquí van todos los estilos CSS*/
-  }
+.imgOffers:hover {
+  opacity: 0.3;
+}
+.textHover {
+  display: none;
+}
+.hover2 .imgOffers:hover ~ .textHover {
+  display: inline-block;
+  color: #fff;
+  position: absolute;
+  z-index: 1;
+  top: 0;
+  padding-left: 10px;
+}
+@media (min-width: 320px) and (max-width: 425px) {
+  /*Aquí van todos los estilos CSS*/
+}
+@media (min-width: 375px) and (max-width: 425px) {
+  /*Aquí van todos los estilos CSS*/
+}
+@media (max-width: 768px) {
+  /*Aquí van todos los estilos CSS*/
+}
+@media (min-width: 960px) and (max-width: 1024px) {
+  /*Aquí van todos los estilos CSS*/
+}
+@media (orientation: landscape) {
+  /*Aquí van todos los estilos CSS*/
+}
+@media (orientation: Portrait) {
+  /*Aquí van todos los estilos CSS*/
+}
 </style>
